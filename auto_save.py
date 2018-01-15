153912d3e3c07b295ee25c1720c77704b33c3c53
SiN13 <gregmoudy@gmail.com>
2018-01-15 15:09:11 -0600

    Some safer logic for getting the auto save filename. Some code to handle filename case changes when saving over existing auto save files.

--- auto_save.py
+++ auto_save.py
@@ -35,14 +35,17 @@ class AutoSaver( wx.Timer ):
 
 	def get_auto_save_file( self ):
 		# Generate a list of the potential auto save filenames.
-		potential_auto_save_files = [ AUTO_SAVE_FILENAME_TEMPLATE.format( x ) for x in range( MAX_AUTO_SAVES ) ]
+		potential_auto_save_files = [ AUTO_SAVE_FILENAME_TEMPLATE.format( x ) for x in range( MAX_AUTO_SAVES ) ] # TODO: Might be able to cache this if it is constant.
 
-		# Generate a list of auto save file names from the potentials list that currently exist.
+		# If all else fails, start with the first one in the list as a default filename.
+		full_local_filename = potential_auto_save_files[ 0 ]
+
+		# Generate a list of auto save file names from the potentials list that currently exist on disk.
 		current_auto_save_files = glob.glob( AUTO_SAVE_FILENAME_TEMPLATE.format( '*' ) )
 		current_auto_save_files = [ x for x in current_auto_save_files if x in potential_auto_save_files ]
 
 		# If the list of current auto saves matches the limit of the number of auto save files maintained...
-		if len( current_auto_save_files ) == MAX_AUTO_SAVES:
+		if len( current_auto_save_files ) >= MAX_AUTO_SAVES:
 			# Sort the current list by modified date and use the first one which should be the oldest file.
 			current_auto_save_files.sort( key = os.path.getmtime )
 			full_local_filename = current_auto_save_files[ 0 ]
@@ -51,7 +54,8 @@ class AutoSaver( wx.Timer ):
 		else:
 			# Get a list of which file names are still available to be written to for the first time and use the first one.
 			available_auto_save_files = [ x for x in potential_auto_save_files if x not in current_auto_save_files ]
-			full_local_filename = available_auto_save_files[ 0 ]
+			if available_auto_save_files:
+				full_local_filename = available_auto_save_files[ 0 ]
 
 		return full_local_filename
 
@@ -61,5 +65,10 @@ class AutoSaver( wx.Timer ):
 		full_local_filename = self.get_auto_save_file( )
 
 		print( '{0} - {1}'.format( time.ctime( ), full_local_filename ) )
+
+		# If the auto save file already exists, remove it before saving.
+		# This makes sure that the filename case is updated to what we are currently trying to save it to.
+		if os.path.exists( full_local_filename ):
+			os.remove( full_local_filename )
 
 		self._save_func( full_local_filename )
